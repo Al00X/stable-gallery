e22d51fc09eb22e83c032bc50808b805d36c9b2c
Ali X <al00x@outlook.com>
2024-06-22 20:58:42 +0330

    INITAH

---
+++ apps/stable-gallery-electron/jest.config.ts
@@ -0,0 +1,11 @@
+/* eslint-disable */
+export default {
+  displayName: 'stable-gallery-electron',
+  preset: '../../jest.preset.js',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/stable-gallery-electron',
+};
---
+++ apps/stable-gallery-electron/src/app/constants.ts
@@ -0,0 +1,4 @@
+export const rendererAppPort = 4200;
+export const rendererAppName = 'stable-gallery'; // options.name.split('-')[0] + '-web'
+export const electronAppName = 'stable-gallery-electron';
+export const updateServerUrl = 'https://deployment-server-url.com'; // TODO: insert your update server url here
